ffda9477f0fe54713f1edcef3e7f37bb76a40345
Julien Faist <j.faist@googlemail.com>
2022-07-27 10:58:33 +0200

    added encounter stuff from before break

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const express = require("express");
+// const uploader = require("./middleware.js").uploader;
+const { uploader } = require("./middleware.js");
 const app = express();
 const db = require("./db.js");
 
@@ -18,6 +20,24 @@ app.get("/table", (req, res) => {
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
+});
+
+//from encounter
+app.post("/image", uploader.single("image"), (req, res) => {
+    //grab the image that was sent [multer]
+    //save it somewhere [multer]
+    //respond to the client - success/failure
+
+    //req.file is created by MUlter if the upload worked!
+    if (req.file) {
+        res.json({
+            success: true,
+        });
+    } else {
+        res.json({
+            success: false,
+        });
+    }
 });
 
 app.listen(8080, () => console.log(`I'm listening.`));
